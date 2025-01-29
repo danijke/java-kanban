ac3fdaddc90dae26fe2007a772cbc47aa9f72363
danijke <dani.for.edu@yandex.kz>
2025-01-29 06:00:50 +0500

    fix: init commit

---
+++ src/service/BaseHttpHandler.java
@@ -0,0 +1,13 @@
+package service;
+
+import com.sun.net.httpserver.*;
+
+import java.io.IOException;
+
+public class BaseHttpHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
